9cd2a46b73e1899cc05c2967d172b604c294c44b
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-06-12 12:18:59 -0500

    added handlebar helper css

--- config/orm.js
+++ config/orm.js
@@ -63,7 +63,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+
   update: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
